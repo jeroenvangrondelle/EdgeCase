fe255ac0226f3896934b28b0b43ef924e09077e2
Jeroen van Grondelle <jeroen@vangrondelle.com>
2023-06-11 17:08:35 +0200

    First code to transform the case to specific sizes

---
+++ docs/specs.md
@@ -0,0 +1,12 @@
+
+IN v0.2
+
+The board width = 47mm
+- Split at 5 + 10 + slack
+The bay width = 23
+- Split at 5 + 47 + added + 3 + slack
+
+The depth = 14
+- Split at 10mm
+The height = 50
+- Split 1t 40
---
+++ src/transform.ts
@@ -0,0 +1,78 @@
+
+import * as fs from 'fs';
+
+
+const OBJ = 'prototypes/Edge Case 0.2.obj';
+const OUT_FILE = "output/transformed.obj";
+
+const board_width=60, 
+    bay_width = 42, 
+    height=77, 
+    depth=30;
+
+
+//Transforming for a 60mm board and 42mm/77 batteryholder
+const transformations = [
+    {
+        dimension:0,
+        insertion: 50,
+        add: board_width-47
+    },
+    {
+        dimension:0,
+        insertion: 5 + board_width + 3 + 20,
+        add:bay_width-23
+    },
+    {
+        dimension:1,
+        insertion: 40,
+        add:height-50
+    },
+
+    {
+        dimension:2,
+        insertion: 10,
+        add:depth-14
+    },
+
+]
+
+
+var obj = fs.readFileSync(OBJ, 'utf-8');
+
+
+
+
+transformations.forEach((transformation)=> {
+    obj = transform(obj, transformation);
+})
+
+
+function transform(obj: string, transformation: any)
+{
+    var OUT = ""
+    const lines = obj.split("\n")
+
+    lines.forEach((line) => {
+        if (line.startsWith("v"))
+        {
+            const cols = line.split(" ");
+            const values = [0.0,0.0,0.0]
+            values[0] = parseFloat(cols[1].trim())
+            values[1] = parseFloat(cols[2].trim())
+            values[2] = parseFloat(cols[3].trim())
+
+            console.log(values)
+
+            if(values[transformation.dimension] > transformation.insertion)
+            {
+                values[transformation.dimension] += transformation.add;
+                line = `v ${values[0]}\t ${values[1]}\t ${values[2]}`;
+            }
+        }
+        OUT +=line +"\n"
+    })
+    return OUT;
+}
+
+fs.writeFileSync(OUT_FILE, obj);
