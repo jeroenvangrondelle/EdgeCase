41241a00684b2d3639bc47abd23fb49b510ed03b
Jeroen van Grondelle <jeroen@vangrondelle.com>
2023-06-19 22:23:27 +0200

    Now also scaling front

--- src/transform.ts
+++ src/transform.ts
@@ -3,12 +3,15 @@ import * as fs from 'fs';
 
 
 const OBJ = 'prototypes/Edge Case 0.4 House.obj';
-const OUT_FILE = "output/transformed.obj";
+const SVG = 'prototypes/Edge Case 0.4 Front.svg';
 
-const A=25.4, 
+const OUT_FILE_OBJ = "output/transformed.obj";
+const OUT_FILE_SVG = "output/transformed.svg";
+
+const A = 25.4, 
     B = 31, 
-    C=60, 
-    D=30;
+    C = 60, 
+    D = 30;
 
 
 
@@ -40,18 +43,65 @@ const transformations = [
 
 
 var obj = fs.readFileSync(OBJ, 'utf-8');
+var svg = fs.readFileSync(SVG, 'utf-8');
 
 
 transformations.forEach((transformation)=> {
     obj = transform_obj(obj, transformation);
 })
 
+transformations.forEach((transformation)=> {
+    svg = transform_svg(svg, transformation);
+})
+
+
+function transform_svg(svg: string, transformation: any)
+{
+    var OUT = "";
+    const mins = get_mins_svg(svg);
+
+
+    const lines = svg.split("\n");
+    lines.forEach((line) => {
+        if (line.startsWith("<path d="))
+        {
+            var path = line.substring(9)
+            const remainder = path.substring(path.indexOf("\""))
+
+            path = path.substring(0, path.indexOf("\""))
+
+            var tokens = path.split(" ")
+            tokens = tokens.map((token, index) => {
+                if(index %3 == 1 && transformation.dimension == 0 && parseFloat(token) - mins[0] > transformation.insertion)
+                {
+                    return parseFloat(token) + transformation.add
+                }
+                else if(index %3 == 2 && transformation.dimension == 1 && parseFloat(token) - mins[1] > transformation.insertion)
+                {
+                    return parseFloat(token) + transformation.add;
+                }
+                else {
+                    return token;
+                }
+
+            })
+            line = "<path d=\"" + tokens.join(" ")+remainder;
+
+            console.log(line);
+            console.log()
+
+        }
+
+        OUT += line +"\n";
+    })
+    return OUT;
+}
 
 function transform_obj(obj: string, transformation: any)
 {
     var OUT = ""
     const lines = obj.split("\n")
-    const mins = get_mins(obj);
+    const mins = get_mins_obj(obj);
 
     lines.forEach((line) => {
         if (line.startsWith("v"))
@@ -75,8 +125,42 @@ function transform_obj(obj: string, transformation: any)
     return OUT;
 }
 
+function get_mins_svg(svg: string)
+{
+    const mins = [1000000,1000000];
 
-function get_mins(obj: string)
+    const lines = svg.split("\n");
+    lines.forEach((line) => {
+        if (line.startsWith("<path d="))
+        {
+            var path = line.substring(9)
+            const remainder = path.substring(path.indexOf("\""))
+
+            path = path.substring(0, path.indexOf("\""))
+
+            var tokens = path.split(" ")
+            tokens.map((token, index) => {
+                if(index %3 == 1)
+                {
+                    var f = parseFloat(token);
+                    if(f < mins[0]) mins[0] = f;
+                }
+                else if(index %3 == 2)
+                {
+                    var f = parseFloat(token);
+                    if(f < mins[1]) mins[1] = f;
+                }
+    
+            })            
+        }
+    })
+    console.log(mins)
+    return mins;
+
+}
+
+
+function get_mins_obj(obj: string)
 {
     const lines = obj.split("\n")
     const mins = [1000000,1000000,1000000];
@@ -105,4 +189,5 @@ function get_mins(obj: string)
 
 
 
-fs.writeFileSync(OUT_FILE, obj);
+fs.writeFileSync(OUT_FILE_OBJ, obj);
+fs.writeFileSync(OUT_FILE_SVG, svg);
